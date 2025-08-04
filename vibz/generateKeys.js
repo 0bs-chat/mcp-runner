bd06f5c66bdbb01701b43a4a7f37f71817cba0da
BarrelOfLube <mantrakp@gmail.com>
2025-08-04 10:14:28 -0700

    feat: add key generation script to create and set JWT_PRIVATE_KEY and JWKS for secure token handling

---
+++ vibz/generateKeys.js
@@ -0,0 +1,29 @@
+import { exportJWK, exportPKCS8, generateKeyPair } from "jose";
+
+// Generate keys
+const keys = await generateKeyPair("RS256", { extractable: true });
+const privateKey = await exportPKCS8(keys.privateKey);
+const publicKey = await exportJWK(keys.publicKey);
+const jwks = JSON.stringify({ keys: [{ use: "sig", ...publicKey }] });
+
+// Set JWT_PRIVATE_KEY
+let result = Bun.spawnSync([
+  "bunx",
+  "convex",
+  "env",
+  "set",
+  "JWT_PRIVATE_KEY",
+  privateKey.trimEnd().replace(/\n/g, " ")
+]);
+console.log(result.stdout.toString(), result.stderr.toString());
+
+// Set JWKS
+result = Bun.spawnSync([
+  "bunx",
+  "convex",
+  "env",
+  "set",
+  "JWKS",
+  jwks
+]);
+console.log(result.stdout.toString(), result.stderr.toString());
